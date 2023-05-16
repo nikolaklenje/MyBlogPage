ddcbccf1d3858f21a8c6bf4ad69d929553344865
nikolaklenje <nikola@prototypenext.com>
2023-05-16 15:39:40 -0400

    Adding baner

---
+++ src/components/layout/baner/index.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react"
+
+
+export const Baner: FC =()=>{
+    return(
+        <div className="text-white flex flex-col">
+    <div className="text-[#64ffda]">
+       <h1>Hi, my name is</h1> 
+         </div>
+    <div>
+      <h2 className="text-6xl">Nikola Stankovic.</h2>  
+        </div>
+<div>
+    <h3 className="text-6xl">I build everything from nothing.</h3></div>
+<div>
+    Front End developer with industry experience building websites and web applications.
+     I specialize in JavaScript and have professional experience working with C++, Typescript and Python.
+     I also have experience working with Next and React.
+</div>
+
+
+            </div>
+    )
+}
