35404db27fa65856793d2c86de39a3f314259aae
nikolaklenje <nikola@prototypenext.com>
2023-06-04 23:46:39 -0400

    added email

--- src/components/layout/conections/index.tsx
+++ src/components/layout/conections/index.tsx
@@ -1,4 +1,6 @@
 import { FC } from "react"
+import style from "/con.module.css"
+import Link from "next/link"
 
 export const Conections: FC = ()=>{
     return(
@@ -12,14 +14,14 @@ export const Conections: FC = ()=>{
                 <li>BBB</li>
                 </ul>
             </div>
-            <div className="fixed bottom-0 right-16">
-                <ul>
-                <li>AAA</li>
-                <li>BBB</li>
-                <li>AAA</li>
-                <li>BBB</li>
-                <li>BBB</li>
-                </ul>
+            <div className="fixed z-50 bottom-0 right-16">
+               <div className="flex-col relative text-[#ccd6f6] hover:text-[#64ffda]">
+                <div className="text-dir-lr">
+                    <Link href="www.google.com">nikola@prototypenext.com</Link>
+                </div>
+                <div className="w-[2px] mt-4 ml-2 h-32 bg-[#ccd6f6]"></div>
+
+                </div>
             </div>
         </div>
     )
