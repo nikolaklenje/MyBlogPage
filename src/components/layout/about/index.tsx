0308a49b46396d4c0f292c287df8def41d08c69e
nikolaklenje <nikola@prototypenext.com>
2023-07-04 16:31:25 -0400

    Create component for About Page

---
+++ src/components/layout/about/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+export const About: FC = () => {
+  return (
+    <>
+      <div>Nikola</div>
+      <div>Nikola</div>
+      <div>Nikola</div>
+      <div>Nikola</div>
+    </>
+  );
+};
--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,5 +1,6 @@
 import { Header } from "./header";
 import { Baner } from "./baner";
 import { Conections } from "./conections";
+import { About } from "./about";
 
-export {Header, Baner, Conections};
+export { Header, Baner, Conections, About };
