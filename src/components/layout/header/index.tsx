0bd54ca01380c0d59a434b137431f4c8aa8ba388
nikolaklenje <nikolaelite88@gmail.com>
2024-06-06 20:49:55 -0400

    Videos to blog

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,29 +1,41 @@
 import { FC } from "react";
 import "animate.css";
+import Link from "next/link";
 
 export const Header: FC = () => {
   return (
     <div className="header-container relative w-full h-16 text-[#64ffda]">
       <div className="flex flex-row center justify-between p-4 pl-16 pr-16">
-        <div className="font-black mt-4 animate__animated animate__fadeInDown">
-          NICODE
-        </div>
+        <Link href="/">
+          {" "}
+          <div className="font-black mt-4 animate__animated animate__fadeInDown">
+            NICODE
+          </div>
+        </Link>
         <div className="flex-row flex ">
-          {/* <ul className="flex flex-row center  mt-4 space-x-4">
+          <ul className="flex flex-row center  mt-4 space-x-4">
             <li className="animate__animated animate__fadeInDownBig">
-              01.<span className="text-white hover:text-[#64ffda]"> Home</span>{" "}
+              01.
+              <Link href="/" className="text-white hover:text-[#64ffda]">
+                {" "}
+                Home
+              </Link>{" "}
             </li>
             <li className="animate__animated animate__fadeInDownBig">
-              02.<span className="text-white hover:text-[#64ffda]"> About</span>{" "}
+              02.
+              <Link href="/blogs" className="text-white hover:text-[#64ffda]">
+                {" "}
+                Blogs
+              </Link>{" "}
             </li>
-            <li className="animate__animated animate__fadeInDownBig ">
+            {/* <li className="animate__animated animate__fadeInDownBig ">
               03.
               <span className="text-white hover:text-[#64ffda]">
                 {" "}
                 Contact
               </span>{" "}
-            </li>
-          </ul> */}
+            </li> */}
+          </ul>
           <div className="border-2 block animate__animated animate__fadeInUpBig  text-[#64ffda] ml-4 mt-0 p-4 rounded-md border-[#64ffda]">
             <a
               href="Nikola_Stankovic_Resume.pdf"
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Conections, Header } from "@/components/layout";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className="flex min-h-screen flex-col items-center w-full ">
+      <Header />
+      <Conections />
+      <Component {...pageProps} />
+    </main>
+  );
 }
