f92226a4a0c5f81e3c131115a9f11024aa04fe73
nikolaklenje <nikolaelite88@gmail.com>
2024-06-20 10:46:03 -0400

    signIn component wrap-up

--- src/components/layout/signIn/index.tsx
+++ src/components/layout/signIn/index.tsx
@@ -17,18 +17,19 @@ export const SignIn: FC = () => {
                   name="name"
                 />
               </div>
-              <div className="relative mb-6">
+              <div className="relative mb-3">
                 <input
                   type="email"
-                  className="tet-white block min-h-[auto] w-full rounded border-2 border-[#64ffda] bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100  data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200"
+                  className="text-white block min-h-[auto] w-full rounded border-2 border-[#64ffda] bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100  data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200"
                   placeholder="Email address"
                   name="email"
                 />
               </div>
+              <p className="text-white mb-4">Dont have an account?</p>
               <input
                 type="Submit"
                 defaultValue="Sign In"
-                className="mb-6 inline-block w-full rounded border-[#64ffda] bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal
+                className="inline-block w-full rounded border-[#64ffda] bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal
                    text-[#64ffda]  shadow-[0_4px_9px_-4px_#64ffda] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]
                     focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
               />
