cddaea0ce43ca1a0a53d241ec32e36346db4f027
nikolaklenje <nikolaelite88@gmail.com>
2024-06-21 14:41:59 -0400

    auth signIn call

--- src/components/layout/signIn/index.tsx
+++ src/components/layout/signIn/index.tsx
@@ -1,8 +1,27 @@
 import { FC, useState } from "react";
-
+import { supabase } from "@/library/supabaseApi";
+import { useRouter } from "next/router";
 export const SignIn: FC = () => {
   const [isSignUp, setIsSignUp] = useState(true);
-
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  const router = useRouter();
+  const signInFlow = async () => {
+    try {
+      const signInAttempt = await supabase.auth.signInWithPassword({
+        email: userEmail,
+        password: userPassword,
+      });
+      const { error } = signInAttempt;
+      if (error) {
+        console.log("SignIn failed", error);
+      } else {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
       {isSignUp ? (
@@ -15,18 +34,24 @@ export const SignIn: FC = () => {
                 <form className="w-full shrink-0 grow-0 basis-auto ">
                   <div className="relative mb-6">
                     <input
-                      type="text"
+                      onChange={(e) => {
+                        setUserEmail(e.target.value);
+                      }}
+                      type="email"
                       className="text-white block min-h-[auto] w-full rounded border-[#64ffda] border-2 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100  data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200"
-                      placeholder="Name"
-                      name="name"
+                      placeholder="email"
+                      name="email"
                     />
                   </div>
                   <div className="relative mb-3">
                     <input
-                      type="email"
+                      onChange={(e) => {
+                        setUserPassword(e.target.value);
+                      }}
+                      type="password"
                       className="text-white block min-h-[auto] w-full rounded border-2 border-[#64ffda] bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100  data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200"
-                      placeholder="Email address"
-                      name="email"
+                      placeholder="password"
+                      name="password"
                     />
                   </div>
                   <p
@@ -35,13 +60,16 @@ export const SignIn: FC = () => {
                   >
                     Dont have an account?
                   </p>
-                  <input
-                    type="Submit"
+                  <button
+                    onClick={signInFlow}
+                    type="submit"
                     defaultValue="Sign In"
                     className="inline-block w-full rounded border-[#64ffda] bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal
                    text-[#64ffda]  shadow-[0_4px_9px_-4px_#64ffda] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]
                     focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
-                  />
+                  >
+                    Sign In
+                  </button>
                 </form>
               </div>
             </section>
