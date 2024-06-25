6fc0965434fcba4d625405f44071f1fb26d72739
nikolaklenje <nikolaelite88@gmail.com>
2024-06-25 18:38:27 -0400

    Password reset component

--- src/components/layout/signIn/index.tsx
+++ src/components/layout/signIn/index.tsx
@@ -53,6 +53,8 @@ export const SignIn: FC = () => {
         <div className="flex flex-col items-center mt-48">
           {" "}
           <h1 className="text-6xl font-semibold text-white ">Sign In</h1>
+          <h1 className="text-6xl font-semibold text-white ">Reset password</h1>
+          <h1 className="text-6xl font-semibold text-white ">Sign Up</h1>
           <div className=" my-20 mx-auto md:px-6">
             <section className="mb-32 text-center">
               <div className="flex flex-wrap justify-center">
@@ -86,7 +88,7 @@ export const SignIn: FC = () => {
                     className="text-white mb-4 cursor-pointer"
                     onClick={() => setIsSignUp(false)}
                   >
-                    Dont have an account?
+                    Don't have an account?
                   </p>
                   <button
                     onClick={(e) => {
@@ -101,6 +103,48 @@ export const SignIn: FC = () => {
                   >
                     Sign In
                   </button>
+                  <p className="text-white my-4 cursor-pointer">
+                    Forgot password?
+                  </p>
+                </form>
+              </div>
+            </section>
+            <section className="mb-32 text-center">
+              <div className="flex flex-wrap justify-center">
+                <form
+                  className="w-full shrink-0 grow-0 basis-auto"
+                  method="post"
+                >
+                  <div className="relative mb-6">
+                    <input
+                      onChange={(e) => {
+                        setUserEmail(e.target.value);
+                      }}
+                      type="email"
+                      className="text-white block min-h-[auto] w-full rounded border-[#64ffda] border-2 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100  data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200"
+                      placeholder="enter your email"
+                      name="email"
+                    />
+                  </div>
+                  <p
+                    className="text-white mb-4 cursor-pointer"
+                    onClick={() => setIsSignUp(false)}
+                  >
+                    Don't have an account?
+                  </p>
+                  <button
+                    onClick={(e) => {
+                      e.preventDefault();
+                      signInFlow();
+                    }}
+                    type="submit"
+                    defaultValue="Reset Password"
+                    className="inline-block w-full rounded border-[#64ffda] bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal
+                   text-[#64ffda]  shadow-[0_4px_9px_-4px_#64ffda] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]
+                    focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+                  >
+                    Reset Password
+                  </button>
                 </form>
               </div>
             </section>
@@ -109,7 +153,6 @@ export const SignIn: FC = () => {
       ) : (
         <div className="flex flex-col  items-center mt-48">
           {" "}
-          <h1 className="text-6xl font-semibold text-white ">Sign Up</h1>
           <div className=" my-20 mx-auto md:px-6">
             <section className="mb-32 text-center">
               <div className="flex flex-wrap justify-center">
