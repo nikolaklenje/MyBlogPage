f06039b4e6cd8199ced62442631f1069d32e5cfb
nikolaklenje <nikolaelite88@gmail.com>
2026-02-21 14:50:45 -0500

    add images

--- src/components/layout/seo/SEO.tsx
+++ src/components/layout/seo/SEO.tsx
@@ -3,26 +3,29 @@ import Head from 'next/head';
 export default function SEO({
   title = 'Nicode',
   description = 'Welcome to Nicode, your go-to platform for the latest in technology, programming, and more.',
-  url = 'https://www.nicode.io/',
-  //image = 'https://www.nicode.io/og-image.png', to be added
+  url = 'https://www.nicode.ai/',
+  image = 'https://nicode.ai/nicode-ai-logo-og.png',
 }) {
-  const fullTitle = title ? `${title} - Nicode` : 'Nicode';
+  const fullTitle = title ? `${title} ` : 'Nicode';
 
   return (
     <Head>
       <title>{fullTitle}</title>
       <meta name="description" content={description} />
       <link rel="canonical" href={url} />
+      <link rel="icon" href="/nicode-ai-logo-square.png" />
+      <link rel="apple-touch-icon" href="/nicode-ai-logo-square.png" />
       <meta property="og:title" content={fullTitle} />
       <meta property="og:description" content={description} />
-      {/* <meta property="og:image" content={image} /> */}
+      <meta property="og:image" content={image} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
       <meta property="og:url" content={url} />
       <meta property="og:type" content="website" />
-
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={fullTitle} />
       <meta name="twitter:description" content={description} />
-      {/* <meta name="twitter:image" content={image} /> */}
+      <meta name="twitter:image" content={image} />
     </Head>
   );
 }
