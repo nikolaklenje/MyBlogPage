b41bb5f73bc43a313af9b100a358538d7b5c9809
nikolaklenje <nikolaelite88@gmail.com>
2026-02-22 16:38:29 -0500

    fix com

--- src/pages/server-sitemap.xml.ts
+++ src/pages/server-sitemap.xml.ts
@@ -2,6 +2,7 @@
 import { getServerSideSitemap, ISitemapField } from 'next-sitemap';
 import fs from 'fs';
 import path from 'path';
+import matter from 'gray-matter';
 
 export async function getServerSideProps() {
   const postsDirectory = path.join(process.cwd(), 'src/listOfBlogs');
@@ -11,10 +12,12 @@ export async function getServerSideProps() {
     .filter((file) => file.endsWith('.md'))
     .map((filename) => {
       const filePath = path.join(postsDirectory, filename);
+      const fileContents = fs.readFileSync(filePath, 'utf8');
+      const { data } = matter(fileContents);
       const stats = fs.statSync(filePath);
 
       return {
-        loc: `https://nicode.ai/blogs/${filename.replace('.md', '')}`,
+        loc: `https://nicode.ai/blogs/${data.id}`,
         lastmod: stats.mtime.toISOString(),
         priority: 0.9,
         changefreq: 'weekly' as const,
